3519a469e8897150b7024cd4b528bb1f8718c35c
56cy <88962938+56cy@users.noreply.github.com>
2023-01-05 18:16:44 +0800

    fix crashing on first run

---
+++ vers/0.7/fastflask.py
@@ -0,0 +1,92 @@
+import flask
+from gevent.pywsgi import WSGIServer
+from flask import request, render_template
+from subprocess import Popen, PIPE
+
+import os
+import json
+import uuid
+import time
+
+app = None
+TIMEOUT = 2 # timeout in seconds
+
+def getAPIResponse(url_path, method, json_data, json_headers):
+
+    rid = str(uuid.uuid4())
+
+    # write to files
+    with open(f".requests/{rid}.rq", "w") as rq_w:
+        rq_w.write(f"{url_path}\n{method}\n{json_data}\n{json_headers}")
+    with open(f".requests/{rid}-complete.rq", "w") as rq_w:
+        pass # just a simple flag to indicate write is complete
+
+    # waiting for response
+    start_time = time.time()
+    while True:
+
+        # timeout
+        if time.time() - start_time >= TIMEOUT:
+            try:
+                os.remove(f".requests/{rid}.rq")
+                os.remove(f".requests/{rid}-complete.rq")
+            except FileNotFoundError: pass
+            return "resp.status = 408", f"<h1>Timeout</h1><p>Request timed out after {TIMEOUT} seconds.</p><br><i>this speedy server is powered by fastflask.</i>"
+
+        # check if result has been written
+        req_folder = set(os.listdir(".requests"))
+        if (f"{rid}-exec.rq" in req_folder) and (f"{rid}-return.rq" in req_folder):
+
+            # check if result is still being written
+            done_writing = False
+            try:
+                os.rename(f".requests/{rid}-exec.rq", f".requests/{rid}-exec.rq")
+                os.rename(f".requests/{rid}-return.rq", f".requests/{rid}-return.rq")
+                done_writing = True
+            except OSError:
+                pass
+            if done_writing:
+                with open(f".requests/{rid}-exec.rq", "r") as rq_r:
+                    to_exec = rq_r.read()
+                with open(f".requests/{rid}-return.rq", "r") as rq_r:
+                    to_return = rq_r.read()
+                os.remove(f".requests/{rid}-exec.rq")
+                os.remove(f".requests/{rid}-return.rq")
+                return to_exec, to_return
+
+def link(_app):
+    global app
+    app = _app
+
+def start(exe_path, host="127.0.0.1", port=5000):
+
+    # open backend process and create directory for handling requests
+    if not ".requests" in os.listdir("."):
+        os.mkdir(".requests")
+    else:
+        for file in os.listdir(".requests"):
+            os.remove(f".requests/{file}")
+    # proc = Popen(exe_path, stdin=PIPE, stdout=PIPE, stderr=PIPE, encoding="UTF8")
+    proc = Popen(exe_path)
+
+    # pass all requests to c++ backend if 404
+    @app.errorhandler(404)
+    def cpp_backend(e):
+        resp = flask.make_response()
+        json_data = None
+        if request.method == "GET" and request.args:
+            json_data = json.dumps(request.args.to_dict())
+        else:
+            json_data = json.dumps(request.json)
+        to_exec, to_return = getAPIResponse(
+            request.path + ("/" if request.path[-1] != "/" else ""),
+            request.method, json_data, json.dumps(dict(request.headers)),
+        )
+        exec(to_exec)
+        if to_return.startswith("render_template"):
+            return eval(to_return)
+        resp.set_data(to_return)
+        return resp
+
+    http_server = WSGIServer((host, port), app)
+    http_server.serve_forever()
